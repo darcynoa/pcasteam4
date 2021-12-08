977d68efcf76b03c7e752dbbaf76157bad3a8e30
darcynoa <darcynoa@msu.edu>
2021-12-08 11:58:20 -0500

    landing page update

---
+++ scripts/script.js
@@ -0,0 +1,17 @@
+const footerHeader = document.querySelector(".footer__header-container");
+
+let lastKnownScrollPosition = 0;
+let ticking = false;
+
+document.addEventListener("scroll", function (e) {
+  lastKnownScrollPosition = window.scrollY;
+
+  if (!ticking) {
+    window.requestAnimationFrame(function () {
+      // console.log(lastKnownScrollPosition);
+      ticking = false;
+    });
+
+    ticking = true;
+  }
+});
